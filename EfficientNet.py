f0b8e6747040d2e0fa0b3b07090250bc260d2b73
kheir <farnaz.kheiri@ontariotechu.ca>
2024-10-01 12:37:17 -0400

    EFNet

--- EfficientNet.py
+++ EfficientNet.py
@@ -1,65 +1,46 @@
-import numpy as np
+
 import tensorflow as tf
-import os
-import pathlib
-import cv2
-# import data_loader
-import pandas as pd
-import pdb
-from split_image import split_image
-from PIL import Image
-import pickle
-from preprocessing import get_labels
-from dataBalancing import balancing
-import random
-
-
-
-
-
-# model dependencies
 from tensorflow.keras.layers import Dense, Rescaling, MaxPooling2D, Flatten, Dropout
-from tensorflow.keras.models import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
 from efficientnet.tfkeras import EfficientNetB0
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 
 
-# Define the model
-base_model = EfficientNetB0(input_shape=(224, 224, 3), include_top=False, weights='imagenet', classes = 2)
-num_classes = 2
-batch_size = 16
-model = Sequential()
-model.add(Rescaling(scale=1./255, input_shape=(224, 224, 3)))
-model.add(base_model)
-for layer in base_model.layers[:-6]:
-    layer.trainable = False
+def EF_model(total_classes, training_data, training_cancer_labels, validation_data, validation_cancer_labels):
+    # Define the model
+    base_model = EfficientNetB0(input_shape=(224, 224, 3), include_top=False, weights='imagenet', classes = 2)
+    num_classes = total_classes
+    batch_size = 16
+    model = Sequential()
+    model.add(Rescaling(scale=1./255, input_shape=(224, 224, 3)))
+    model.add(base_model)
+    for layer in base_model.layers[:-6]:
+        layer.trainable = False
 
-# Add a pooling layer, flatten the output and add a dense layer with 1024 units
-model.add(MaxPooling2D((7,7)))
-model.add(Flatten())
-model.add(Dense(1024, activation="relu"))
-# model.add(BatchNormalization())
+    # Add a pooling layer, flatten the output and add a dense layer with 1024 units
+    model.add(MaxPooling2D((7,7)))
+    model.add(Flatten())
+    model.add(Dense(1024, activation="relu"))
+    # model.add(BatchNormalization())
 
-# # Add regularization
-model.add(Dropout(rate=0.3))
-model.add(Dense(256, kernel_regularizer=tf.keras.regularizers.l2(0.01), activation="relu"))
-# model.add(Dropout(rate=0.2))
-# Add the output layer
-model.add(Dense(2, activation="softmax"))
+    # # Add regularization
+    model.add(Dropout(rate=0.3))
+    model.add(Dense(256, kernel_regularizer=tf.keras.regularizers.l2(0.01), activation="relu"))
+    # model.add(Dropout(rate=0.2))
+    # Add the output layer
+    model.add(Dense(2, activation="softmax"))
 
-# # Compile the model
-optimizer = Adam(learning_rate=0.001)
-model.compile(optimizer=optimizer, loss='BinaryCrossentropy', metrics=['accuracy'])
+    # # Compile the model
+    optimizer = Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='BinaryCrossentropy', metrics=['accuracy'])
 
-# Train the model
-history = model.fit(n_training_data,
-                    n_training_cancer_labels,
-                    steps_per_epoch=len(n_training_data) // batch_size,
-                    epochs=5,
-                    shuffle=True,
-                    validation_data = (n_validation_data, n_validation_cancer_labels)
-                   )
+    # Train the model
+    history = model.fit(training_data,
+                        training_cancer_labels,
+                        steps_per_epoch=len(training_data) // batch_size,
+                        epochs=5,
+                        shuffle=True,
+                        validation_data = (validation_data, validation_cancer_labels)
+                       )
+
+    return model
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 
 from preprocessing import get_labels
 from dataBalancing import balancing
+import numpy as np
+from EfficientNet import EF_model
 
+# define variables
 
-
-# define data root
 data_root = "C:/Users/kheir/Downloads/Shortcut_Learning/data_set"
-train_filtered_data_dic = {"University of Pittsburgh": 100, "Johns Hopkins": 100, "International Genomics Consortium": 100}
+total_slides= 100
+selected_centers_dic = {"Johns Hopkins": total_slides, "Asterand": total_slides, "Indivumed": total_slides, "Roswell Park": total_slides}
+cancers = ["Lung Squamous Cell Carcinoma", "Lung Adenocarcinoma"]
+num_classes = len(cancers)
+
 
 # read dataset
 print("****************reading train data****************")
@@ -14,3 +19,42 @@ print("****************reading train data****************")
 # this method preprocess the original patches (1024x1024) and generates the sub-patches (224x224)
 data_images, data_cancer_labels,  data_center_labels, slidenames, filenames = get_labels(data_root)
 
+# this method balances the data over both cancer types and data centers
+reshaped_balanced_images, balanced_cancer_labels, balanced_center_labels, balanced_slidenames, filename = balancing(selected_centers_dic, data_images, data_center_labels, data_cancer_labels, cancers, slidenames, filenames)
+
+# Manual dataSet Shuffeling ######################################################################
+num_samples = data_images.shape[0]
+# create a random permutation of indices
+indices = np.random.permutation(num_samples)
+
+# # the permutation to shuffle the dataset and labels
+shuffled_reshaped_balanced_images = reshaped_balanced_images[indices]
+shuffled_balanced_cancer_labels = balanced_cancer_labels[indices]
+shuffled_balanced_center_labels = np.array(balanced_center_labels)[indices]
+
+
+
+training_size = int(len(shuffled_balanced_center_labels) * 0.8)   # 80% of the array
+validation_size = int(len(shuffled_balanced_center_labels) * 0.1)   # 10% of the array
+
+# Split the array into three parts
+# training
+training_data = shuffled_reshaped_balanced_images[:training_size]
+training_cancer_labels = shuffled_balanced_cancer_labels[:training_size]
+training_center_labels = shuffled_balanced_center_labels[:training_size]
+
+
+#validation
+validation_data = shuffled_reshaped_balanced_images[training_size:training_size+validation_size]
+validation_cancer_labels = shuffled_balanced_cancer_labels[training_size:training_size+validation_size]
+validation_center_labels = shuffled_balanced_center_labels[training_size:training_size+validation_size]
+
+#test
+test_data = shuffled_reshaped_balanced_images[training_size+validation_size:]
+test_cancer_labels = shuffled_balanced_cancer_labels[training_size+validation_size:]
+test_center_labels = shuffled_balanced_center_labels[training_size+validation_size:]
+
+######################################################################
+
+# Model Training
+trained_model =  EF_model(num_classes, training_data, training_cancer_labels, validation_data, validation_cancer_labels)
