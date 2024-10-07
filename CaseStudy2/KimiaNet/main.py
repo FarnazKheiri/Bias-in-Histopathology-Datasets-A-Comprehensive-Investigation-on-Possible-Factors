eef1cac957c76ad7ecd0391d0ad64098296d9d53
kheir <farnaz.kheiri@ontariotechu.ca>
2024-10-07 18:37:15 -0400

    kim3

--- CaseStudy2/KimiaNet/main.py
+++ CaseStudy2/KimiaNet/main.py
@@ -1,7 +1,7 @@
 from KimiaNet_Featute_Filtering import KimiaNet_Features_Filtering
 from CaseStudy2.test_in_test import test_in_test
 from CaseStudy2.test_in_train import test_in_train
-from CaseStudy2.excluded_test_in_test import excluded_test_in_train
+from CaseStudy2.excluded_test_in_test import excluded_test_in_test
 
 
 ############################################################load festures of KimiaNet
@@ -28,8 +28,8 @@ print("test_in_train_center_acc" + "is" + test_in_train_center_acc)
 
 ############################################################# test_in_test search with exclusion condition
 #cancer classification
-ex_test_in_test_cancer_acc = excluded_test_in_train(k=3,test_features=x_test, test_labels=y_test_cancer,test_slide_names=test_slide_names)
+ex_test_in_test_cancer_acc = excluded_test_in_test(k=3,test_features=x_test, test_labels=y_test_cancer,test_slide_names=test_slide_names)
 print("ex_test_in_test_cancer_acc" + "is" + ex_test_in_test_cancer_acc)
 #center classification
-ex_test_in_test_center_acc = excluded_test_in_train(k=3, test_features=x_test, test_labels=y_test_center,test_slide_names=test_slide_names)
+ex_test_in_test_center_acc = excluded_test_in_test(k=3, test_features=x_test, test_labels=y_test_center,test_slide_names=test_slide_names)
 print("ex_test_in_test_center_acc" + "is" + ex_test_in_test_center_acc)
--- CaseStudy2/excluded_test_in_test.py
+++ CaseStudy2/excluded_test_in_test.py
@@ -4,7 +4,7 @@ from sklearn import metrics
 import pandas as pd
 
 
-def excluded_test_in_train(k,test_features,test_labels,test_slide_names):
+def excluded_test_in_test(k,test_features,test_labels,test_slide_names):
 
     classifier = KNeighborsClassifier(n_neighbors=k)
 
@@ -22,7 +22,7 @@ def excluded_test_in_train(k,test_features,test_labels,test_slide_names):
         print(f"Processing slide {i}...")
 
         # Find indices for the current slide in the test set
-        SlIndex = np.where(test_slide_names == slide)[0]
+        SlIndex = np.where(np.array(test_slide_names) == slide)[0]
 
         # Extract test data and labels for the current slide
         tst = np.array(test_features)[SlIndex]
